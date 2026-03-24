a619a7f271917acdec29c6d9aded08c972876341
Kelvin Isievwore <kelvinator4leo@gmail.com>
2026-03-24 02:20:57 +0100

    updated exercises

--- submission/08.sh
+++ submission/08.sh
@@ -1,2 +1,2 @@
-# What is the total output value of this partially signed transaction in satoshis
+# What is the receiver's address in this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
