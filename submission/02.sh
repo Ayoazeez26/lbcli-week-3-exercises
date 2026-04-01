13d0e842ce70fb6f5e54554e91967f26a94a4869
Ayoazeez26 <44169294+Ayoazeez26@users.noreply.github.com>
2026-04-01 15:17:52 +0100

    feat: completed week 3 exercises

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,5 @@
 # Create a native segwit address and get the public key from the address.
+
+NEW_ADDR=$(bitcoin-cli -regtest getnewaddress "" "bech32")
+
+bitcoin-cli -regtest -named getaddressinfo address=$NEW_ADDR | jq -r '.pubkey'
